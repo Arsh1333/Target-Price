08face935bcc3212ed23fe7bc3dc2bfc23dd3188
Arsh <pawar.arsh1333@gmail.com>
2023-11-13 14:51:33 +0530

    added styles and forms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="tpc-calc">
+        <form className="calc-form">
+          <label>Enter Current Price:</label>
+          <input type="number" />
+          <label>Enter Current PE ratio:</label>
+          <input type="number" />
+          <label>Enter Forward PE ratio:</label>
+          <input type="number" />
+          <button className="btn">Target Price</button>
+        </form>
+      </div>
     </div>
   );
 }
