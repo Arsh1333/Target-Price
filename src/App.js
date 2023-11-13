3e98dc352be4002da5103a02bba29d56add3e29e
Arsh <pawar.arsh1333@gmail.com>
2023-11-13 16:13:43 +0530

    Target price + Forward PE calculator

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { useState } from "react";
 
 function App() {
   const [cp, setCp] = useState("");
-  const [displayedCp, setDisplayedCp] = useState("");
   const [pe, setPe] = useState("");
-  const [displayedPe, setDisplayedPe] = useState("");
   const [fpe, setFpe] = useState("");
-  const [displayedFpe, setDisplayedFpe] = useState("");
+  const [displayedAns, setDisplayedAns] = useState("");
+  const [eps, setEps] = useState("");
+  const [displayedEpsAns, setDisplayedEpsAns] = useState("");
 
   const cpHandler = (e) => {
     setCp(e.target.value);
@@ -18,15 +18,27 @@ function App() {
   const fpeHandler = (e) => {
     setFpe(e.target.value);
   };
+  const epsHandler = (e) => {
+    setEps(e.target.value);
+  };
+  let epsAns = 0;
   let ans = 0;
   const handleDisplayButtonClick = (e) => {
     e.preventDefault();
     ans = (cp * pe) / fpe;
-    alert(`Target Price : ${ans}`);
+    //alert(`Target Price : ${ans}`);
+    setDisplayedAns(`The terget price for given stock: ${ans}`);
+  };
+  const handleButtonClick = (e) => {
+    e.preventDefault();
+    epsAns = cp / eps;
+    //alert(epsAns);
+    setDisplayedEpsAns(`The EPS for given stock: ${epsAns}`);
   };
   return (
     <div className="App">
       <div className="tpc-calc">
+        <h1 className="ans">TARGET PRICE CALCULATOR</h1>
         <form className="calc-form">
           <label>Enter Current Price:</label>
           <input type="number" onChange={cpHandler} />
@@ -38,6 +50,20 @@ function App() {
             Target Price
           </button>
         </form>
+        <p className="ans">{displayedAns}</p>
+
+        <h1 className="ans">FORWARD PE CALCULATOR</h1>
+
+        <form className="fpe-form">
+          <label>Enter Current Price:</label>
+          <input type="number" onChange={cpHandler} />
+          <label>Enter Current EPS:</label>
+          <input type="number" onChange={epsHandler} />
+          <button className="btn" onClick={handleButtonClick}>
+            Forward PE
+          </button>
+        </form>
+        <p className="ans">{displayedEpsAns}</p>
       </div>
     </div>
   );
